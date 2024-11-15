944921199e2e1b63636af04c29e5043483938995
Italo Rocha <italorochaj@icloud.com>
2024-11-15 00:03:45 -0300

    commit de novas classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import br.com.desadio.dominio.Curso;
+import br.com.desadio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso(); // 'new' instancia a classe.
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso(); // 'new' instancia a classe.
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição curso JavaScript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
